b0e7e7e446e41dd62a9b45840fa3ac8e4a2e2c1a
Jakub Narolewski <izowiuz@gmail.com>
2026-09-09 15:25:27 +0200

    nvim: bump

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    keys = {
+        { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",                         desc = "Workspace diagnostics" },
+        { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",            desc = "Buffer diagnostics" },
+        { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>",                 desc = "Symbols outline" },
+        { "<leader>xL", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",  desc = "LSP defs / refs / impls" },
+        { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",                              desc = "Quickfix list" },
+        { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",                             desc = "Location list" },
+    },
+    opts = {},
+}
